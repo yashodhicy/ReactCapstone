457485f60f3c37452cd116a36bc745bd215ff718
yashodhi Chathurangi <yashodhichathurangi94@gmail.com>
2023-06-27 20:38:19 +0300

    fetch data from API

--- src/components/country.js
+++ src/components/country.js
@@ -1,7 +1,18 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { FetchEvents } from '../redux/events/eventsSlice';
+import { useParams } from 'react-router';
+
+
 const Country = () => {
-    return (
-        <p>this is Country</p>
-    )
-}
+    const {id} = useParams();
+    const allEvents = useSelector((state) => state.events.events);
+    const selectedEvent = allEvents.find((event) => event.id === id);
+
+  return (
+    <p>{selectedEvent?.name}</p>
+  )
+    
+};
 
 export default Country;
--- src/components/home.js
+++ src/components/home.js
@@ -1,13 +1,42 @@
-import { Link } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
+import worldImage from "./world.png"
+import { useDispatch, useSelector } from "react-redux";
+import { FetchEvents } from "../redux/events/eventsSlice";
+import { useEffect } from "react";
+
+
+const importAll = (r) => {
+    let images = {};
+    r.keys().forEach((key) => {
+      const imageName = key.replace('./', '');
+      images[imageName] = r(key);
+    });
+    return images;
+  };
+
+//   const images = importAll(require.context('../images', false, /\.(png|jpe?g|svg)$/));
+
 
 const Home = () => {
+    const allEvents = useSelector((state) => state.events.events);
+    const dispatch = useDispatch();
+    useEffect(()=>{
+        dispatch(FetchEvents());
+    },[]);
     return (
         <>
         <div>
-           <p>image</p> 
+            <img src={ worldImage } alt="world" width='100%' height={200}/>
         </div>
         <div>
-            <Link to='/country'>country</Link>
+        {/* {Object.keys(images).map((imageName) => { */}
+        { allEvents.map((event) =>{
+            // const imageNameNew = imageName.replace(/\.[^.]+$/, '');
+            return (
+            <Link to= {`./country/${event.id}`} key={event.id}><img key={event.name} src={event.image} alt={event.name} width={60} height={60} /></Link>
+            
+            )}
+        )}
         </div>
         </>
     )
