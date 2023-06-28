dbf18ef9ceca450814c38a72a65f513c4f6c8231
yashodhi Chathurangi <yashodhichathurangi94@gmail.com>
2023-06-28 08:21:00 +0300

    filter necessary data

--- src/components/home.js
+++ src/components/home.js
@@ -1,45 +1,58 @@
 import { Link, useSearchParams } from "react-router-dom";
-import worldImage from "./world.png"
+import worldImage from "../images/world.png";
 import { useDispatch, useSelector } from "react-redux";
 import { FetchEvents } from "../redux/events/eventsSlice";
 import { useEffect } from "react";
-
+import Filterdata from "./filterdata";
 
 const importAll = (r) => {
-    let images = {};
-    r.keys().forEach((key) => {
-      const imageName = key.replace('./', '');
-      images[imageName] = r(key);
-    });
-    return images;
-  };
-
-//   const images = importAll(require.context('../images', false, /\.(png|jpe?g|svg)$/));
-
+  let images = {};
+  r.keys().forEach((key) => {
+    const imageName = key.replace("./", "");
+    images[imageName] = r(key);
+  });
+  return images;
+};
 
 const Home = () => {
-    const allEvents = useSelector((state) => state.events.events);
-    const dispatch = useDispatch();
-    useEffect(()=>{
-        dispatch(FetchEvents());
-    },[]);
-    return (
-        <>
-        <div>
-            <img src={ worldImage } alt="world" width='100%' height={200}/>
-        </div>
-        <div>
-        {/* {Object.keys(images).map((imageName) => { */}
-        { allEvents.map((event) =>{
-            // const imageNameNew = imageName.replace(/\.[^.]+$/, '');
-            return (
-            <Link to= {`./country/${event.id}`} key={event.id}><img key={event.name} src={event.image} alt={event.name} width={60} height={60} /></Link>
-            
-            )}
-        )}
-        </div>
-        </>
-    )
-}
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(FetchEvents());
+  }, []);
+
+  const allEvents = Filterdata();
+  console.log(allEvents);
+
+  return (
+    <>
+      <div>
+        <img src={worldImage} alt="world" width="100%" height={200} />
+      </div>
+      <div>
+        {allEvents.map((event) => {
+          // const imageNameNew = imageName.replace(/\.[^.]+$/, '');
+          return (
+            <Link to={`./country/${event.id}`} key={event.id}>
+              <div
+                key={event.name}
+                style={{
+                  backgroundImage: `url(${event.image})`,
+                  backgroundSize: "cover",
+                  backgroundPosition: "center",
+                  width: '50%',
+                  height: 100,
+                }}
+              >
+                <h2>{event.name}</h2>
+                <p>{event.ticketLimit}</p>
+                <p>{event.date}</p>
+              </div>
+            </Link>
+          );
+        })}
+      </div>
+    </>
+  );
+};
 
 export default Home;
--- src/redux/events/eventsSlice.js
+++ src/redux/events/eventsSlice.js
@@ -4,7 +4,7 @@ export const FetchEvents = createAsyncThunk(
     'Country/FetchEvents',
     
     async(_,thunkAPI) => {
-        const url = 'https://app.ticketmaster.com/discovery/v2/events.json?size=6&countryCode=US&classificationName=Professional&apikey=7KBAIQZR5JiH4pE3CMdHPToUvYySPND8';
+        const url = 'https://app.ticketmaster.com/discovery/v2/events.json?size=6&countryCode=CA&classificationName=Professional&apikey=7KBAIQZR5JiH4pE3CMdHPToUvYySPND8';
         try {
             const resp = await fetch(url);
             return resp.json();
@@ -34,17 +34,7 @@ const EventSlice = createSlice ( {
             state.isLoading = false;
             const data = action.payload._embedded.events;
             console.log(data);
-            const eventsnew = data.map((event) => ({
-                id: event.id,
-                name: event.name,
-                genre: event.classifications[0].segment.name,
-                image: event.images[0].url,
-                date : event.dates.start.dateTime,
-                price: event.priceRanges[0].min+' ' + event.priceRanges[0].currency,
-
-            }))
-            state.events = eventsnew;
-            console.log(state.events);
+            state.events = data;
         })
         .addCase(FetchEvents.rejected, (state) => {
             state.isLoading = false;
