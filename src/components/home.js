4b636286612740a039794ecec70efbd3d991f1f1
yashodhi Chathurangi <yashodhichathurangi94@gmail.com>
2023-06-28 19:23:35 +0300

    add linters and fix linter errors

--- src/components/home.js
+++ src/components/home.js
@@ -1,19 +1,10 @@
-import { Link } from "react-router-dom";
-import worldImage from "../images/world.png";
-import { useDispatch} from "react-redux";
-import { FetchEvents } from "../redux/events/eventsSlice";
-import { useEffect } from "react";
-import Filterdata from "./filterdata";
-import "./home.css";
-
-const importAll = (r) => {
-  let images = {};
-  r.keys().forEach((key) => {
-    const imageName = key.replace("./", "");
-    images[imageName] = r(key);
-  });
-  return images;
-};
+import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import worldImage from '../images/world.png';
+import { FetchEvents } from '../redux/events/eventsSlice';
+import Filterdata from './filterdata';
+import './home.css';
 
 const Home = () => {
   const dispatch = useDispatch();
@@ -32,50 +23,51 @@ const Home = () => {
       </div>
       <p className="divider">Details by event</p>
       <div className="events">
-        {allEvents.map((event) => {
-          return (
-            <Link to={`./event/${event.id}`} key={event.id}>
+        {allEvents.map((event) => (
+          <Link to={`./event/${event.id}`} key={event.id}>
+            <div
+              className="event"
+              key={event.name}
+              style={{
+                position: 'relative',
+                backgroundImage: `url(${event.image})`,
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+              }}
+            >
               <div
-                className="event"
-                key={event.name}
+                className="overlay"
                 style={{
-                  position: "relative",
-                  backgroundImage: `url(${event.image})`,
-                  backgroundSize: "cover",
-                  backgroundPosition: "center",
+                  position: 'absolute',
+                  top: 0,
+                  left: 0,
+                  width: '100%',
+                  height: '100%',
+                  backgroundColor: 'rgb(223, 71, 130, 0.9)',
+                  zIndex: 1,
+                }}
+              />
+              <article
+                className="event-details"
+                style={{
+                  position: 'relative',
+                  bottom: '0px',
+                  right: '0px',
+                  color: 'white',
+                  zIndex: 2,
+                  width: '70%',
                 }}
               >
-                <div
-                  className="overlay"
-                  style={{
-                    position: "absolute",
-                    top: 0,
-                    left: 0,
-                    width: "100%",
-                    height: "100%",
-                    backgroundColor: "rgb(223, 71, 130, 0.9)",
-                    zIndex: 1,
-                  }}
-                ></div>
-                <article
-                  className="event-details"
-                  style={{
-                    position: "relative",
-                    bottom: "0px",
-                    right: "0px",
-                    color: "white",
-                    zIndex: 2,
-                    width: "70%",
-                  }}
-                >
-                  <h4 style={{ textTransform: "uppercase"}}>{event.name}</h4>
-                  <p>{event.price}</p>
-                  <p>Tickets limit: {event.ticketLimit}</p>
-                </article>
-              </div>
-            </Link>
-          );
-        })}
+                <h4 style={{ textTransform: 'uppercase' }}>{event.name}</h4>
+                <p>{event.price}</p>
+                <p>
+                  Tickets limit:
+                  {event.ticketLimit}
+                </p>
+              </article>
+            </div>
+          </Link>
+        ))}
       </div>
     </>
   );
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,33 +1,33 @@
-import { Nav, Navbar } from "react-bootstrap";
-import { IoIosArrowBack, IoIosMic, IoIosSettings } from "react-icons/io";
-import { Link, useLocation } from "react-router-dom";
-import "./navbar.css";
+import { Nav, Navbar } from 'react-bootstrap';
+import { IoIosArrowBack, IoIosMic, IoIosSettings } from 'react-icons/io';
+import { Link, useLocation } from 'react-router-dom';
+import './navbar.css';
 
 const Navigation = () => {
   const location = useLocation();
-  let text = "Home";
+  let text = 'Home';
 
-  if (location.pathname === "/") {
-    text = "Home";
-  } else if (location.pathname.startsWith("/event/")) {
-    text = "event details";
+  if (location.pathname === '/') {
+    text = 'Home';
+  } else if (location.pathname.startsWith('/event/')) {
+    text = 'event details';
   }
 
   return (
     <Navbar bg="dark" variant="dark">
-    <Nav className="mr-auto">
-      <Nav.Link as={Link} to="/">
-        <IoIosArrowBack fill="white" />
-      </Nav.Link>
-    </Nav>
-    <Navbar.Brand className="mx-auto">
-      <p className="text-center">{text}</p>
-    </Navbar.Brand>
-    <Nav>
-      <IoIosMic fill="white" />
-      <IoIosSettings fill="white" />
-    </Nav>
-  </Navbar>
+      <Nav className="mr-auto">
+        <Nav.Link as={Link} to="/">
+          <IoIosArrowBack fill="white" />
+        </Nav.Link>
+      </Nav>
+      <Navbar.Brand className="mx-auto">
+        <p className="text-center">{text}</p>
+      </Navbar.Brand>
+      <Nav>
+        <IoIosMic fill="white" />
+        <IoIosSettings fill="white" />
+      </Nav>
+    </Navbar>
   );
 };
 
